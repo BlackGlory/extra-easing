fb64898b60e268d08b35de58056e7af0bce55bf4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-16 16:20:01 +0000

    fix(power): prevent power is negative value

--- __tests__/utils/power.spec.ts
+++ __tests__/utils/power.spec.ts
@@ -1,10 +1,27 @@
-import { test, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
+import { getError } from 'return-style'
 import { power } from '@utils/power.js'
 
-test('power', () => {
-  const fn = power(2)
+describe('power', () => {
+  test('power > 0', () => {
+    const fn = power(2)
 
-  expect(fn(0)).toBe(0)
-  expect(fn(0.5)).toBe(0.5 ** 2)
-  expect(fn(1)).toBe(1)
+    expect(fn(0)).toBe(0)
+    expect(fn(0.5)).toBe(0.5 ** 2)
+    expect(fn(1)).toBe(1)
+  })
+
+  test('power = 0', () => {
+    const fn = power(0)
+
+    expect(fn(0)).toBe(1)
+    expect(fn(0.5)).toBe(1)
+    expect(fn(1)).toBe(1)
+  })
+
+  test('power < 0', () => {
+    const error = getError(() => power(-1))
+
+    expect(error).toBeInstanceOf(Error)
+  })
 })
--- src/utils/power.ts
+++ src/utils/power.ts
@@ -1,8 +1,11 @@
+import { assert } from '@blackglory/prelude'
 import { EasingFunction } from '@src/types.js'
 
 /**
  * @see https://learn.microsoft.com/en-us/dotnet/api/system.windows.media.animation.powerease
  */
 export function power(power: number): EasingFunction {
+  assert(power >= 0, 'The power must be greater than or equal to 0')
+
   return normalizedTime => normalizedTime ** power
 }
